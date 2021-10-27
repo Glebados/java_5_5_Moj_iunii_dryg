cfe615500f88249b1e160c4f6886da62e48ce1e7
Glebados <glebrudenko22@gmail.com>
2021-10-27 09:49:47 +0200

    Task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -3,6 +3,11 @@ package it.campe;
 public class Main {
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        String a = "Год рождения моего друга : ";
+        int b = 2021;
+        int c = 22;
+        int d = b-c;
+        System.out.print(a);
+        System.out.print(d);
     }
 }
